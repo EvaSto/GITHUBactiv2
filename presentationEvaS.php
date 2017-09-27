aae537c058e633031387ab886e48df472bc98847
Eva Sto <evastonem@yahoo.com>
2017-09-27 21:00:38 +0200

    Mise en place d'une liste sous HTML à completer

--- presentationEvaS.php
+++ presentationEvaS.php
@@ -3,3 +3,9 @@
 echo "Salut je m'appelle Eva et j'apprends à coder et utiliser tous les merveilleux logiciels qui nous entoure";
 
 ?>
+
+<li>
+	<ul>Passion</ul>
+	<ul>Loisir</ul>
+	<ul>Vie quotidienne</ul>
+</li>
